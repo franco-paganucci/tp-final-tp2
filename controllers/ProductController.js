5ff8dfbe9306502afabf6f7a997a2d81670ff3e8
Andres Subero <andressuberoh@gmail.com>
2024-11-09 13:27:21 -0300

    product model created and controller updated

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,61 +1,64 @@
 import ProductService from "../services/ProductService.js";
 class ProductController {
+
     service = new ProductService();
 
     getAllProducts = async (req, res) => {
+        try {
         const data = await this.service.getAllProducts();
         res.status(200).send({ success: true, message: data });
-    // try {
-    //   const data = await this.service.getAllProducts();
-    //   res.status(200).send({ success: true, message: data });
-    // } catch (error) {
-    //   res.status(400).send({ success: false, message: error.message });
-    // }
-  };
+        } catch (error) {
+        res.status(400).send({ success: false, message: error.message });
+        }
+    };
 
-  getProductById = async (req, res) => {
+    getProductById = async (req, res) => {
     try {
-      const { id } = req.params;
-      const data = await this.service.getProductById(id);
-      res.status(200).send({ success: true, message: data });
+        const { id } = req.params;
+        const data = await this.service.getProductById(id);
+        res.status(200).send({ success: true, message: data });
     } catch (error) {
-      res.status(400).send({ success: false, message: error.message });
+        res.status(400).send({ success: false, message: error.message });
     }
-  };
-  createProduct = async (req, res) => {
-    try {
-      const { name, price, stock, id } = req.body;
-      const data = await this.service.createProduct({
-        name,
-        price,
-        stock,
-        id
-      });
-      res.status(200).send({ success: true, message: data });
-    } catch (error) {
-      res.status(400).send({ success: false, message: error.message });
-    }
-  };
+    };
 
-  updateProduct = async (req, res) => {
-    try {
-      const { name, price, stock} = req.body;
-      const { id } = req.params;
-      const data = await this.service.updateProduct({id, name, price, stock});
-      res.status(200).send({ success: true, message: data });
-    } catch (error) {
-      res.status(400).send({ success: false, message: error.message });
-    }
-  };
-  deleteProduct = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const data = await this.service.deleteProduct(id);
-      res.status(200).send({ success: true, message: data });
-    } catch (error) {
-      res.status(400).send({ success: false, message: error.message });
-    }
-  };
+    createProduct = async (req, res) => {
+        try {
+            const { name, price, stock, description, image } = req.body;
+            const data = await this.service.createProduct({
+            name,
+            price,
+            stock,
+            description,
+            image,
+            active: true
+            });
+            res.status(200).send({ success: true, message: data });
+        } catch (error) {
+            res.status(400).send({ success: false, message: error });
+        }
+    };
+
+    updateProduct = async (req, res) => {
+        try {
+            const { name, price, stock, image} = req.body;
+            const { id } = req.params;
+            const data = await this.service.updateProduct({id, name, price, stock, image});
+            res.status(200).send({ success: true, message: data });
+        } catch (error) {
+            res.status(400).send({ success: false, message: error.message });
+        }
+    };
+
+    deleteProduct = async (req, res) => {
+        try {
+            const { id } = req.params;
+            const data = await this.service.deleteProduct(id);
+            res.status(200).send({ success: true, message: data });
+        } catch (error) {
+            res.status(400).send({ success: false, message: error.message });
+        }
+    };
 
 }
 
