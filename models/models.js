0afe37ff88872bca8c765552e4ac735c6cebb310
Andres Subero <andressuberoh@gmail.com>
2024-11-10 15:04:22 -0300

    create hooks to calculate price

--- models/models.js
+++ models/models.js
@@ -7,23 +7,47 @@ import OrderItem from "./OrderItem.js";
 User.hasMany(Order, { foreignKey: 'userId' });
 Order.belongsTo(User, { foreignKey: 'userId' })
 Order.hasMany(OrderItem, {
-    foreignKey: 'orderId',  // Aquí defines la clave foránea en camelCase
+    foreignKey: 'orderId',
     onDelete: 'CASCADE',
-  });
+});
+
 OrderItem.belongsTo(Order, { foreignKey: 'orderId' })
 OrderItem.belongsTo(Product, { foreignKey: 'productId'})
 Product.hasMany(OrderItem, { foreignKey: 'productId' })
 
-// Order.addHook('afterSave', async (order) => {
-//     const totalPrice = await OrderItem.sum('price', {
-//       where: { orderId: order.id },
-//     });
-  
-//     order.price = totalPrice || 0.00;
-//     await order.save();
-// });
+const updateOrderPrice = async (orderItem, options) => {
+    try {
+      const order = await Order.findByPk(orderItem.orderId);
+      if (order) {
+        const orderItems = await order.getOrderItems();
+        const total = orderItems.reduce((acc, item) => { return acc + item.price; }, 0);
+        order.price = total;
+        await order.save()
+      } else {
+        throw new Error('Order not found');
+      }
+    } catch (error) {
+      console.error('Error calculating price for Order:', error);
+    }
+}
 
-// export {Role, User}
+const setOrderItemPrice = async (orderItem, options) => {
+    try {
+      const product = await Product.findByPk(orderItem.productId);
+      if (product) {
+        orderItem.price = product.price * orderItem.quantity;
+      } else {
+        throw new Error('Product not found');
+      }
+    } catch (error) {
+      console.error('Error calculating price for OrderItem:', error);
+    }
+}
+
+OrderItem.addHook('beforeCreate', setOrderItemPrice);
+
+OrderItem.addHook('afterCreate', updateOrderPrice);
+
 export { Product, User, Order, OrderItem }
 
 
