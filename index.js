714d67a8270e7dcc5314355894f860cbb04c9198
Andres Subero <andressuberoh@gmail.com>
2024-11-18 22:06:10 -0300

    filter for products

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(routes);
 app.use(notFound);
 
-await connection.sync({ alter: true, force: true });
+await connection.sync();
 await roleSeed();
 await userSeed();
 await productSeed();
